f33dcbf2dbd7e2c888d002ec5b09a3cd18e60009
CIECs20171103196 <1614211282@qq.com>
2018-09-29 10:06:36 +0800

    giii
    gg

--- swiftTest94188/main.swift
+++ swiftTest94188/main.swift
@@ -7,6 +7,33 @@
 //
 
 import Foundation
-
+var i=0
+var j=0
+var x=0
+var a:[Int]=[10,9,8,7,6,5,4,3,2,1]
+var temp=0
+for i in 0 ..< 9
+{
+    for j in 0 ..< (9-i)
+    {
+        if(a[j]>a[j+1])
+        {
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
+            
+        }
+    }
+}
+for x in 0..<10 {
+    print("\(a[x])",terminator: " ")
+}
+/*
+func runoob(a: int) -> int {
+    if(a>0){
+    return(a+runoob(a:"a-1"))
+    }
+}
 print ("welcome to github")
-print ("你访问的对象不存在")
+print (runoob(a:"4"))
+*/
