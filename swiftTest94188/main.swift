38fde0a23ead8ae7a345310408cdc41f1a3f7cb1
CIECs20171103196 <1614211282@qq.com>
2018-09-29 08:43:35 +0800

    Initial Commit

---
+++ swiftTest94188/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftTest94188
+//
+//  Created by s20171103196 on 2018/9/29.
+//  Copyright © 2018年 s20171103196. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
